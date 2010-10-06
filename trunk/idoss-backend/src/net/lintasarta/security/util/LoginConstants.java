882fb1b1685e71c853774c8b0eed87895929bcdb
antonius.sianturi@gmail.com <antonius.sianturi@gmail.com@59eea869-dece-0caf-8a99-c029baa0b33b>
2010-10-06 04:07:11 +0000

    Login Role

--- trunk/idoss-backend/src/net/lintasarta/security/util/LoginConstants.java
+++ trunk/idoss-backend/src/net/lintasarta/security/util/LoginConstants.java
@@ -7,13 +7,15 @@ package net.lintasarta.security.util;
  * Time: 4:30:21 PM
  */
 public class LoginConstants {
-    public static final String USER = "user";
     public static final String MUSER = "muser";
     public static final String GMUSER = "gmuser";
     public static final String AMDUK = "amduk";
     public static final String MDUK = "mduk";
     public static final String GMDUK = "gmduk";
-    public static final String HELPD = "helpd";
-    public static final String PELAK = "pelak";
-    public static final String MONITOR = "monitor";
+
+    // Role Modul Pengaduan
+    public static final String IDOSS_HELPDESK_ADUAN = "IDOSS HELPDESK ADUAN";       // helpd
+    public static final String IDOSS_INPUT_ADUAN = "IDOSS INPUT ADUAN";             // user
+    public static final String IDOSS_MONITORING_ADUAN = "IDOSS MONITORING ADUAN";   // monitoring
+    public static final String IDOSS_PELAKSANA_ADUAN = "IDOSS PELAKSANA ADUAN";     // pelak
 }
