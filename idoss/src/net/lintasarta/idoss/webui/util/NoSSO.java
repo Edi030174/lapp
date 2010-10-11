6694a929d096a3887f11f7e7e9edd11ef9e35f63
antonius.sianturi <antonius.sianturi@gmail.com>
2010-10-11 10:01:01 +0000

    Login Role Pengaduan

---
+++ idoss/src/net/lintasarta/idoss/webui/util/NoSSO.java
@@ -0,0 +1,40 @@
+package net.lintasarta.idoss.webui.util;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Antonius
+ * Date: Oct 11, 2010
+ * Time: 4:27:05 PM
+ */
+public class NoSSO {
+    private String employeeNo;
+    private String userName;
+    private String employeeName;
+
+    public NoSSO() {
+    }
+
+    public String getEmployeeNo() {
+        return employeeNo;
+    }
+
+    public void setEmployeeNo(String employeeNo) {
+        this.employeeNo = employeeNo;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+}
